f9eb5f555ab18f79dc36102178ca39650fd485e3
Jesse <Jesse>
2024-02-25 13:48:43 -0800

    migrate to lsio container

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+
 docker-compose down
-docker image rm ghcr.io/home-assistant/home-assistant:stable
-docker image prune -f
+docker-compose pull
+mkdir -p ./volumes/config/custom_components/
 sudo zip -r opensprinkler-backup-$(date +%Y-%m-%d) ./volumes/config/custom_components/opensprinkler
 cd hass-opensprinkler
 git pull
 cd ..
 sudo rm -rf ./volumes/config/custom_components/opensprinkler
 cp -r hass-opensprinkler/custom_components/opensprinkler ./volumes/config/custom_components/
-docker image pull ghcr.io/home-assistant/home-assistant:stable
+docker image prune -f
 COMPOSE_HTTP_TIMEOUT=720 docker-compose up -d

eac468ec02bc11d6a7a0af548822f7d7492701be
Jesse <Jesse>
2024-02-25 14:15:57 -0800

    reverse proxy

--- update.sh
+++ update.sh
@@ -2,12 +2,12 @@
 
 docker-compose down
 docker-compose pull
-mkdir -p ./volumes/config/custom_components/
-sudo zip -r opensprinkler-backup-$(date +%Y-%m-%d) ./volumes/config/custom_components/opensprinkler
+mkdir -p ./volumes/ha/config/custom_components/
+sudo zip -r opensprinkler-backup-$(date +%Y-%m-%d) ./volumes/cha/onfig/custom_components/opensprinkler
 cd hass-opensprinkler
 git pull
 cd ..
-sudo rm -rf ./volumes/config/custom_components/opensprinkler
-cp -r hass-opensprinkler/custom_components/opensprinkler ./volumes/config/custom_components/
+sudo rm -rf ./volumes/ha/config/custom_components/opensprinkler
+cp -r hass-opensprinkler/custom_components/opensprinkler ./volumes/ha/config/custom_components/
 docker image prune -f
 COMPOSE_HTTP_TIMEOUT=720 docker-compose up -d

49f98ce518ea22be1067645eba25ab50979a98bb
Jesse <jesse>
2024-10-31 15:29:08 -0500

    update config and update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-docker-compose down
-docker-compose pull
-mkdir -p ./volumes/ha/config/custom_components/
-sudo zip -r opensprinkler-backup-$(date +%Y-%m-%d) ./volumes/ha/config/custom_components/opensprinkler
-cd hass-opensprinkler
-git checkout master
-git pull
-cd ..
-sudo rm -rf ./volumes/ha/config/custom_components/opensprinkler
-cp -r hass-opensprinkler/custom_components/opensprinkler ./volumes/ha/config/custom_components/
+docker compose down
+docker compose pull
+#mkdir -p ./volumes/ha/config/custom_components/
+#sudo zip -r opensprinkler-backup-$(date +%Y-%m-%d) ./volumes/ha/config/custom_components/opensprinkler
+#cd hass-opensprinkler
+#git checkout master
+#git pull
+#cd ..
+#sudo rm -rf ./volumes/ha/config/custom_components/opensprinkler
+#cp -r hass-opensprinkler/custom_components/opensprinkler ./volumes/ha/config/custom_components/
 docker image prune -f
-COMPOSE_HTTP_TIMEOUT=1200 docker-compose up -d
+docker compose up -d

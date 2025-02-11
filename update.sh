e8bcc1f2666e91b82dde849de56bc81b01b5478b
Jesse <Jesse>
2025-02-10 23:14:52 -0800

    add mushroom

--- update.sh
+++ update.sh
@@ -19,5 +19,13 @@ cd ..
 sudo rm -rf ./volumes/ha/config/customer_components/frigate
 cp -r frigate-hass-integration/custom_components/frigate ./volumes/ha/config/custom_components/
 
+cd lovelace-mushroom
+git fetch --tags
+LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+cd ..
+mkdir -p ./volumes/ha/config/www
+ASSET_URL="https://github.com/piitaya/lovelace-mushroom/releases/download/$LATEST_TAG/mushroom.js"
+curl -L -o "./volumes/ha/config/www/mushroom.js" "$ASSET_URL"
+
 docker image prune -f
 docker compose up -d

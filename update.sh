0ac2af29fc3509ccc56077189e01ea028b95d53c
Jesse <Jesse>
2025-02-15 22:43:12 -0800

    add bubble card

--- update.sh
+++ update.sh
@@ -6,8 +6,7 @@ docker compose pull
 git submodule init
 git submodule update
 
-mkdir -p ./volumes/ha/config/custom_components
-
+# mkdir -p ./volumes/ha/config/custom_components
 #sudo zip -r opensprinkler-backup-$(date +%Y-%m-%d) ./volumes/ha/config/custom_components/opensprinkler
 #cd hass-opensprinkler
 #git pull
@@ -15,6 +14,7 @@ mkdir -p ./volumes/ha/config/custom_components
 #sudo rm -rf ./volumes/ha/config/custom_components/opensprinkler
 #cp -r hass-opensprinkler/custom_components/opensprinkler ./volumes/ha/config/custom_components/
 
+mkdir -p ./volumes/ha/config/custom_components
 sudo zip -r frigate-hass-integration-backup-$(date +%Y-%m-%d) ./volumes/ha/config/custom_components/frigate
 cd frigate-hass-integration
 git pull
@@ -29,6 +29,13 @@ cd ..
 mkdir -p ./volumes/ha/config/www
 ASSET_URL="https://github.com/piitaya/lovelace-mushroom/releases/download/$LATEST_TAG/mushroom.js"
 curl -L -o "./volumes/ha/config/www/mushroom.js" "$ASSET_URL"
+
+cd Bubble-Card
+git pull
+cd ..
+mkdir -p ./volumes/ha/config/www
+cp ./Bubble-Card/dist/bubble-card.js volumes/ha/config/www/
+cp ./Bubble-Card/dist/bubble-pop-up-fix.js volumes/ha/config/www/
 
 docker image prune -f
 docker compose up -d

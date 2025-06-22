ecb57c9cabb119c7d5c56a02046d66fdc211d018
Jesse <79180615+jesse-public@users.noreply.github.com>
2025-06-22 00:50:01 -0700

    update

--- update.sh
+++ update.sh
@@ -6,14 +6,6 @@ docker compose pull
 git submodule init
 git submodule update
 
-# mkdir -p ./volumes/ha/config/custom_components
-#sudo zip -r opensprinkler-backup-$(date +%Y-%m-%d) ./volumes/ha/config/custom_components/opensprinkler
-#cd hass-opensprinkler
-#git pull
-#cd ..
-#sudo rm -rf ./volumes/ha/config/custom_components/opensprinkler
-#cp -r hass-opensprinkler/custom_components/opensprinkler ./volumes/ha/config/custom_components/
-
 mkdir -p ./volumes/ha/config/custom_components
 sudo zip -r frigate-hass-integration-backup-$(date +%Y-%m-%d) ./volumes/ha/config/custom_components/frigate
 cd frigate-hass-integration
@@ -29,13 +21,6 @@ cd ..
 mkdir -p ./volumes/ha/config/www
 ASSET_URL="https://github.com/piitaya/lovelace-mushroom/releases/download/$LATEST_TAG/mushroom.js"
 curl -L -o "./volumes/ha/config/www/mushroom.js" "$ASSET_URL"
-
-cd Bubble-Card
-git pull
-cd ..
-mkdir -p ./volumes/ha/config/www
-cp ./Bubble-Card/dist/bubble-card.js volumes/ha/config/www/
-cp ./Bubble-Card/dist/bubble-pop-up-fix.js volumes/ha/config/www/
 
 docker image prune -f
 COMPOSE_BAKE=true docker compose up --build -d
